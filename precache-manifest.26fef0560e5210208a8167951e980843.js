70a9acca7bb4d373ad3c8635f0088e2e3e203f11
BurlakovEP <burlakov_e_p@mail.ru>
2019-11-11 13:58:34 +0600

    Updates

---
+++ precache-manifest.26fef0560e5210208a8167951e980843.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8421b603519085b3a551f93e34feafb2",
+    "url": "/Calendar/index.html"
+  },
+  {
+    "revision": "913aaa01ac989ed22360",
+    "url": "/Calendar/static/css/main.a125fb11.chunk.css"
+  },
+  {
+    "revision": "27ae5dd9f86e52e6541e",
+    "url": "/Calendar/static/js/2.692f73ec.chunk.js"
+  },
+  {
+    "revision": "913aaa01ac989ed22360",
+    "url": "/Calendar/static/js/main.e8cd91ea.chunk.js"
+  },
+  {
+    "revision": "fc3b9c46c82451764c55",
+    "url": "/Calendar/static/js/runtime-main.914ab0b7.js"
+  }
+]);
