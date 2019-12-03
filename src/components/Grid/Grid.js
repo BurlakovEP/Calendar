757d269b04bdd7cf60eb6665df8f92018286cb54
BurlakovEP <burlakov_e_p@mail.ru>
2019-12-03 14:46:17 +0600

    Added description to file README.md

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import './Grid.scss';
+import React from "react";
+import "./Grid.scss";
 
 export default class Grid extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      daysOfWeek: this.getDaysOfWeek(),
+      daysOfWeek: this.getDaysOfWeek()
     };
   }
 
   getDaysOfWeek = () => {
-    let days = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+    let days = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
     let arr = [];
     for (let i = 0; i < days.length; i++) {
       let obj = {
         string: days[i],
         number: i + 1,
-        className: 'grid__cell grid__cell_head',
+        className: "grid__cell grid__cell_head"
       };
 
       if (i === days.length - 1) obj.number = 0;
@@ -32,18 +32,18 @@ export default class Grid extends React.Component {
       <table className="grid">
         <thead className="grid__head">
           <tr className="grid__row grid__row_head">
-            {this.state.daysOfWeek.map((cell) => (
-              <td className={cell.className} key={cell.number}>
+            {this.state.daysOfWeek.map((cell, index) => (
+              <td className={cell.className} key={index}>
                 {cell.string}
               </td>
             ))}
           </tr>
         </thead>
         <tbody className="grid__body">
-          {days.map((row) => (
-            <tr className="grid__row" key={days.indexOf(row)}>
-              {row.map((cell) => (
-                <td className={cell.className} key={cell.id}>
+          {days.map((row, index) => (
+            <tr className="grid__row" key={index}>
+              {row.map((cell, index) => (
+                <td className={cell.className} key={index}>
                   {cell.number}
                 </td>
               ))}
